abb87167ca34e21df6e44ed14f447635bee05ed5
Mathias Talo <mathias.talo@outlook.com>
2023-09-26 23:15:02 +0300

    Added page title and icon

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import 'react'
 import './App.sass'
 import Weather from './Weather'
 import Forecast from './Forecast'
+import {Helmet} from 'react-helmet'
 
 function App() {
 
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Mathias56k Weather Site</title>
+        <link rel="Page Icon" href="assets/page-icon.png" />
+      </Helmet>
       <Weather />
       <Forecast />
     </>
