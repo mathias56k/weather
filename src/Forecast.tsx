c714001e77f57c1914deae6f7356539e4351473a
Mathias Talo <mathias.talo@outlook.com>
2023-09-28 23:41:46 +0300

    Fixed TS problems

--- src/Forecast.tsx
+++ src/Forecast.tsx
@@ -3,11 +3,8 @@ import axios from 'axios';
 import './Forecast.sass';
 import config from './config';
 
-import clearsky from './assets/04d.svg'
-
 const Forecast = ({ city }: any) => {
-  const [forecastData, setForecastData] = useState(null);
-  const [weatherIcon, setWeatherIcon] : any = useState(null);
+  const [forecastData, setForecastData] = useState<{ [key: string]: any } | null>(null);
 
   const fetchData = async () => {
     try {
@@ -27,9 +24,9 @@ const Forecast = ({ city }: any) => {
 
   const renderForecastData = () => {
     if (forecastData) {
-      const groupedForecastData = {};
+      const groupedForecastData: { [key: string]: any[] } = {};
 
-      forecastData.list.forEach((item) => {
+      forecastData.list.forEach((item: any) => {
         const date = new Date(item.dt * 1000);
         const day = date.toLocaleDateString('en-US', { weekday: 'long' });
         const dayNumber = date.getDate();
@@ -52,10 +49,11 @@ const Forecast = ({ city }: any) => {
         return (
           <div className='forecast-day-container' key={day}>
             <div className='forecast-day'>
-              <p className='forecast-day-number'>{dailyForecasts[0].dayNumber}</p> <p className='forecast-day-text'>{day}</p>
+              <p className='forecast-day-number'>{dailyForecasts[0].dayNumber}</p>
+              <p className='forecast-day-text'>{day}</p>
             </div>
             <div className='forecast-hour-container'>
-              {dailyForecasts.map((forecast, index) => (
+              {dailyForecasts.map((forecast: any, index: any) => (
                 <div key={index} className='forecast-hour-item'>
                   <p className='fc-item forecast-hour-text'>{forecast.hour}</p>
                   <p className='fc-item forecast-hour-temp-text'>{forecast.temp}°C</p>
